9042fdfc62928bbe702a5cfdb0189c45dc804e4d
Jeremy Moya Herbas <jmoyah@cibergestionperu.com>
2025-12-05 23:39:08 -0500

    fix# Upload 2

--- src/app/pages/clientes/clientes.component.ts
+++ src/app/pages/clientes/clientes.component.ts
@@ -2,17 +2,23 @@ import { Component, inject } from '@angular/core';
 import { AccesoService } from '../../services/acceso.service';
 import { Cliente } from '../../interfaces/Cliente';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-clientes',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, FormsModule],
   templateUrl: './clientes.component.html',
   styleUrl: './clientes.component.css'
 })
 export class ClientesComponent {
+
   private accesoService = inject(AccesoService)
   public listaClientes: Cliente[] = []
+
+  modalAbierto: boolean = false;
+  clienteSeleccionado: Cliente | null = null;
+  nuevoEstado: string = '';
 
   constructor() {
     this.accesoService.getListaClientes().subscribe({
@@ -26,4 +32,31 @@ export class ClientesComponent {
       }
     })
   }
+
+  abrirModal(cliente: Cliente) {
+    this.clienteSeleccionado = cliente;
+    // this.nuevoEstado = cliente.ESTADO;
+    this.nuevoEstado = 'inactivo';
+    this.modalAbierto = true;
+  }
+
+  cerrarModal() {
+    this.modalAbierto = false;
+  }
+
+  guardarEstado() {
+    if (this.clienteSeleccionado) {
+      // this.clienteSeleccionado.ESTADO = this.nuevoEstado;
+
+      // Aquí puedes llamar a tu API para actualizarlo en la BD
+      // this.accesoService.actualizarEstadoCliente(this.clienteSeleccionado.ID, this.nuevoEstado)
+      //   .subscribe({
+      //     next: () => console.log("Estado actualizado correctamente"),
+      //     error: (err) => console.log(err)
+      //   });
+    }
+
+    this.modalAbierto = false;
+  }
+
 }
